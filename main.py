53df325d84e4d95ab2b36e57d8d4530ee541bcbf
Frieso Turkstra <friesoturkstra@gmail.com>
2023-02-01 11:27:29 +0100

    added window

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("uwu")
-print("uwu")
+import pygame
+from pygame.locals import *
+
+pygame.init()
+win = pygame.display.set_mode((400, 400))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            break
