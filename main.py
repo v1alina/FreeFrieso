40a06b1ddf5969fe53801c1c003fc0840fe10d88
Frieso Turkstra <friesoturkstra@gmail.com>
2023-02-01 11:56:17 +0100

    basic pygame structure

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import pygame
 from pygame.locals import *
 
-pygame.init()
-pygame.quit()
+class Game:
+    def __init__(self):
+        self.running = True
+        self.window_width = 400
+        self.window_height = 400
+
+    def run(self):
+        pygame.init()
+
+        # create window
+        window = pygame.display.set_mode((self.window_width, self.window_height))
+
+        # main game loop
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.running = False
+                    pygame.quit()
+
+
+class Player:
+    pass
+
+class Arrow:
+    pass
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
